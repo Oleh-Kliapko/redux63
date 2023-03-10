b445f3db74667b370d06f0a9a17ac9c30e9dd267
Oleh Kliapko <goldclub1977@gmail.com>
2023-03-10 20:26:33 +0200

    task 1 is DONE

---
+++ src/components/SearchForm/SearchForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
+import { FormBtn, InputSearch, SearchFormStyled } from './SearchForm.styled';
+
+export const SearchForm = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit(query);
+    setQuery('');
+  };
+
+  return (
+    <SearchFormStyled onSubmit={handleSubmit}>
+      <FormBtn type="submit">
+        <FiSearch size="16px" />
+      </FormBtn>
+      <InputSearch
+        onChange={e => setQuery(e.currentTarget.value)}
+        placeholder="What do you want to write?"
+        name="search"
+        required
+        value={query}
+        autoFocus
+      />
+    </SearchFormStyled>
+  );
+};
